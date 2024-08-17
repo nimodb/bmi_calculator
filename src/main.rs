34b832ef00d94a6249b82b20846a5d287adccb2e
Nima Saadati <nimodbdev@gmail.com>
2024-08-17 13:26:21 +0400

    Add basic BMI calculator logic and main func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+fn calculator_bmi(weight: f32, height: f32) -> f32 {
+    weight / (height * height)
+}
+
 fn main() {
     println!("Welcome to the BMI Calculator!");
+
+    let weight = 66.8; // in kg
+    let height = 1.748; // in m
+
+    let bmi = calculator_bmi(weight, height);
+
+    println!("Your BMI is: {:.3}", bmi);
 }
